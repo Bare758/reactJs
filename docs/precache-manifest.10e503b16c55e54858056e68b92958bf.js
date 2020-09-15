3b97ab6d04f75d3b35570a538d2521de403c2dba
Bare758 <Abdulahial413@gmail.co>
2020-09-15 10:03:18 +0200

    Update

---
+++ docs/precache-manifest.10e503b16c55e54858056e68b92958bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad097bf974eb115c81d6560a153c0ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47af5999265d7663d1fc",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "87ee4ac382e307e70e3d",
+    "url": "/static/js/2.ff0f0576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff0f0576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47af5999265d7663d1fc",
+    "url": "/static/js/main.81d2e392.chunk.js"
+  },
+  {
+    "revision": "fe2af3bd6869259f6b81",
+    "url": "/static/js/runtime-main.49925162.js"
+  }
+]);
