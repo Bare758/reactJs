9135ff383a768e886d86fcac0fbd27c3980b520c
Bare758 <Abdulahial413@gmail.co>
2020-09-15 09:49:15 +0200

    TodoList

---
+++ src/Todo.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+class Todo extends Component {
+  getStyle() {
+    return {
+      backgroundColor: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px dotted #ccc",
+      textDecoration: this.props.todoProp.completed ? "line-through" : "none",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          onChange={this.props.markCompleteProp2.bind(
+            this,
+            this.props.todoProp.id
+          )}
+        ></input>
+
+        <div style={this.getStyle()}>{this.props.todoProp.title}</div>
+      </div>
+    );
+  }
+}
+
+Todo.propTypes = {
+  todoProp: propTypes.object.isRequired,
+};
+
+export default Todo;
